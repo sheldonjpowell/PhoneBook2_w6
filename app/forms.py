b7f933f0c01ded2e3514dec56c22f7e39bbc0f0a
sheldonjpowell <sheldonjpowell@yahoo.com>
2022-03-31 21:25:17 -0500

    ading alerts

--- app/forms.py
+++ app/forms.py
@@ -4,8 +4,8 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 class SignUpForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
     username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
--- app/routes.py
+++ app/routes.py
@@ -21,21 +21,27 @@ def index():
 def signup():
     title = 'Sign Up'
     form = SignUpForm()
-    # check if a post request and that the form is valid
     if form.validate_on_submit():
-        # Get data from the validated form
-        email = form.email.data
-        username = form.username.data
-        password = form.password.data
-        # Create a new user instance with form data
-        new_user = User(email=email, username=username, password=password)
-        return redirect(url_for('index'))
 
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+
+        users_with_that_info = User.query.filter((User.username==username)|(User.email==email)).all()
+        if users_with_that_info:
+            flash(f"There is already a username and/or email", "danger")
+            return redirect(url_for('signup'))
+            
+        new_user = User(username=username, email=email, password=password)
+
+        flash(f"{new_user.username} has succesfully signed up.", "success")
+        return redirect(url_for('index'))
+        
     return render_template('signup.html', title=title, form=form)
 
 
 @app.route('/address', methods=["GET", "POST"])
-def register_address():
+def address():
     title = 'Address'
     form = AddressForm()
     if form.validate_on_submit():
